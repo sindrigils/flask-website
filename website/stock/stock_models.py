e396282282a6ba03bc0be07e2152e49f5cfff7f6
Sindri Gils <sindrigils02@gmail.com>
2023-03-17 20:40:38 +0000

    users now have balance and gets updated

--- website/stock/stock_models.py
+++ website/stock/stock_models.py
@@ -6,6 +6,8 @@ from website import app
 import requests
 import json
 import matplotlib.pyplot as plt
+from datetime import datetime
+import matplotlib.dates as mdates
 
 load_dotenv()
 api_key = key=os.getenv("API_KEY")
@@ -25,28 +27,59 @@ def get_stock_price_by_ticker(stock_ticker: str):
 
 
 
+# def plot_a_graph(symbol):
+#     interval = "60min"
+#     url = f'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol={symbol}&interval={interval}&apikey={api_key}'
+
+#     # Make a request to the API and parse the response as JSON
+#     response = requests.get(url)
+#     data = json.loads(response.text)
+
+#     # Extract the stock prices data from the JSON response
+#     time_series = data['Time Series (60min)']
+#     dates = []
+#     prices = []
+
+#     for date in time_series:
+#         dates.append(date)
+#         prices.append(float(time_series[date]['4. close']))
+
+#     # Plot the stock prices data using Matplotlib
+#     plt.plot(dates, prices)
+#     plt.xlabel('Time')
+#     plt.ylabel('Price')
+#     plt.title(f'Stock Prices for {symbol}')
+
+#     # Save the graph as a PNG file
+#     filename = f'{symbol}_graph.png'
+#     filepath = os.path.join(app.root_path, 'static', 'graphs', filename)
+#     plt.savefig(filepath)
+
+#     plt.close()
+
+#     return filename
+
 def plot_a_graph(symbol):
-    interval = "60min"
-    url = f'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol={symbol}&interval={interval}&apikey={api_key}'
+    
+    data, meta_data = ts.get_intraday(symbol=symbol, interval="60min")
 
-    # Make a request to the API and parse the response as JSON
-    response = requests.get(url)
-    data = json.loads(response.text)
+    dates = data.index.tolist()
+    prices = data['4. close'].tolist()
 
-    # Extract the stock prices data from the JSON response
-    time_series = data['Time Series (60min)']
-    dates = []
-    prices = []
-
-    for date in time_series:
-        dates.append(date)
-        prices.append(float(time_series[date]['4. close']))
+    # Convert the date strings to datetime objects
+    dates = [datetime.strptime(str(date), '%Y-%m-%d %H:%M:%S') for date in dates]
 
     # Plot the stock prices data using Matplotlib
-    plt.plot(dates, prices)
-    plt.xlabel('Time')
-    plt.ylabel('Price')
-    plt.title(f'Stock Prices for {symbol}')
+    fig, ax = plt.subplots()
+    ax.plot(dates, prices)
+    ax.set_xlabel('Time')
+    ax.set_ylabel('Price')
+    ax.set_title(f'Stock Prices for {symbol}')
+
+    # Format the timestamps on the x-axis
+    date_fmt = mdates.DateFormatter('%Y-%m-%d %H:%M:%S')
+    ax.xaxis.set_major_formatter(date_fmt)
+    fig.autofmt_xdate()
 
     # Save the graph as a PNG file
     filename = f'{symbol}_graph.png'
@@ -56,3 +89,8 @@ def plot_a_graph(symbol):
     plt.close()
 
     return filename
+
+
+
+
+
--- website/views.py
+++ website/views.py
@@ -4,6 +4,7 @@ from website import db
 from website.models import Stock, Account
 from website.forms import StockTickerForm, PurchaseStockForm, SellStockForm, AccountForm
 from website.stock.stock_models import get_stock_price_by_ticker, plot_a_graph
+from copy import deepcopy
 
 views = Blueprint("views", __name__)
 
@@ -35,31 +36,45 @@ def buy_stock_page(stock_ticker, stock_price):
     filename = plot_a_graph(stock_ticker)
 
     if form.validate_on_submit():
+
+        if not current_user.can_buy(total_cost=float(stock_price)*float(form.shares.data)):
+            flash(message=f"You dont have enough money to buy this amount of shares!", category="danger")
+            return redirect(url_for("views.profile_page"))
+        
         buy_stock = Stock(
             ticker=stock_ticker,
             average_price=stock_price,
             shares=form.shares.data,
+            cost_basis=float(form.shares.data) * float(stock_price),
             user_id=current_user.id)
         
         owned_stock = Stock.query.filter_by(user_id=current_user.id, ticker=stock_ticker).first() # check if the user already owns this stock if so than add up his shares
         if owned_stock:
-            old_total_shares = owned_stock.shares
-            old_average_price = owned_stock.average_price
 
+            # update the stock
+            old_total_shares = owned_stock.shares
             new_total_shares = old_total_shares + form.shares.data
-            new_average_price = (old_average_price * old_total_shares + float(form.shares.data) * float(stock_price)) / new_total_shares
+
+            old_cost_basis = owned_stock.cost_basis
+            new_cost_basis = old_cost_basis + (form.shares.data * float(stock_price))
+
+            new_average_price = new_cost_basis / new_total_shares
 
             owned_stock.shares = new_total_shares
             owned_stock.average_price = new_average_price
-
-            db.session.commit()
+            owned_stock.cost_basis = new_cost_basis
 
         else:
             db.session.add(buy_stock)
-            db.session.commit()
+
+        # update the account
+        account = Account.query.filter_by(user_id=current_user.id).first()
+        account.balance -= float(form.shares.data) * float(stock_price) 
+
+        db.session.commit()
 
         flash(message=f"You have bought the stock successfully!", category="success")
-        return redirect(url_for("views.stock_ticker_page"))
+        return redirect(url_for("views.profile_page"))
 
     return render_template("buy_stock.html", form=form, stock_ticker=stock_ticker, stock_price=stock_price, filename=filename)
 
@@ -71,14 +86,14 @@ def profile_page():
     form = AccountForm()
 
     if form.validate_on_submit():
-        account_created = Account(account_owner=current_user.id)
+        account_created = Account(user_id=current_user.id)
 
         db.session.add(account_created)
         db.session.commit()
 
 
     owned_stocks = Stock.query.filter_by(user_id=current_user.id)
-    has_account = Account.query.filter_by(account_owner=current_user.id).first()
+    has_account = Account.query.filter_by(user_id=current_user.id).first()
 
     return render_template("profile.html", stocks=owned_stocks, form=form, account=has_account)
 
@@ -88,16 +103,33 @@ def sell_stock_page():
     form = SellStockForm()
 
     if form.validate_on_submit():
-        stock_ticker = form.stock_ticker.data
+        stock_ticker = form.stock_ticker.data.upper()
         stock_shares = form.shares.data
 
         if current_user.can_sell(stock_ticker=stock_ticker, shares=stock_shares):
             owned_stock = Stock.query.filter_by(user_id=current_user.id, ticker=stock_ticker).first()
 
-            owned_stock.shares = owned_stock.shares - stock_shares
+            # update the stock
+            old_total_shares = owned_stock.shares
+            new_total_shares = old_total_shares - stock_shares
 
-            if owned_stock.shares == 0: # if he sold all of his stocks
+            if new_total_shares == 0: # if he sold all of his stocks
                 db.session.delete(owned_stock)
+
+            else:
+                old_average_price = owned_stock.average_price
+                new_average_price = (owned_stock.cost_basis - (stock_shares * old_average_price)) / new_total_shares
+
+                new_cost_basis = new_average_price * new_total_shares
+
+                owned_stock.shares = new_total_shares
+                owned_stock.average_price = new_average_price
+                owned_stock.cost_basis = new_cost_basis
+
+            # update the balance
+            account = Account.query.filter_by(user_id=current_user.id).first()
+            current_stock_price = get_stock_price_by_ticker(stock_ticker)
+            account.balance += stock_shares * current_stock_price
 
             db.session.commit()
             flash(message=f"You successfully sold {stock_shares} amount of shares at {owned_stock.average_price}$", category="success")
